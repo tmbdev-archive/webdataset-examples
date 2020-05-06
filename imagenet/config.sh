a06b140b119dfba24f977cbc85cc0cdc9c46a293
Tom <tmbdev+github@gmail.com>
2020-05-05 23:39:40 -0700

    updated config.sh with comment

--- imagenet/config.sh
+++ imagenet/config.sh
@@ -1,3 +1,8 @@
+# Configuration options for the ./run script.
+# You only need to change these if you are using Docker.
+# If you just want to run the Python scripts directly,
+# configure the paths with --imagenet and --shards
+
 # path to the raw ImageNet dataset (suitable for use with
 # torchvision.datasets.ImageNet)
 imagenet="/mdata/imagenet-raw"
